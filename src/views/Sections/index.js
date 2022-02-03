18ff70c7f2bd1d6ef0148b27f79f6817d58fa13b
Rohan Lekhwani <rohanlekhwani@ymail.com>
2022-02-03 21:58:19 +0530

    feat: add skills section

--- src/views/Sections/index.js
+++ src/views/Sections/index.js
@@ -4,3 +4,4 @@ export { default as Projects } from "./Projects";
 export { default as Education } from "./Education";
 export { default as Me } from "./Me";
 export { default as Publications } from "./Publications";
+export { default as Skills } from "./Skills";
