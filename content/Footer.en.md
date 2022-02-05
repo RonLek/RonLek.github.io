08a3519c262a7b86b5947f76835f405bd03f82aa
Rohan Lekhwani <rohanlekhwani@ymail.com>
2022-02-05 11:52:19 +0530

    feat: add footer

--- content/Footer.en.md
+++ content/Footer.en.md
@@ -1,12 +1,3 @@
 ---
-copyright: "Copyright © Start Bootstrap 2020"
-social: {
-  twitter: "#",
-  github: "#",
-  medium: "#"
-}
-privacyText: "Privacy Policy"
-privacyHref: "#!"
-termsText: "Terms of Use"
-termsHref: "#!"
+copyright: "Copyright © Rohan Lekhwani 2022"
 ---
--- src/views/Footer/Footer.jsx
+++ src/views/Footer/Footer.jsx
@@ -2,41 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { Container, Row, Col } from "react-bootstrap";
-import * as SocialIcons from "components/SocialIcons";
+import Love from "../../../content/assets/images/logos/love.png";
 
 const Footer = ({ frontmatter }) => {
   if (!frontmatter) {
     return null;
   }
 
-  const {
-    copyright,
-    privacyHref,
-    privacyText,
-    termsHref,
-    termsText,
-    // social: { github, linkedin, medium, twitter, scholar },
-  } = frontmatter;
+  const { copyright } = frontmatter;
 
   return (
     <footer className="footer py-3">
       <Container>
+        <Row className="d-flex flex-row justify-content-center align-items-center">
+          <p>
+            {" "}
+            Made with <img alt="Love" src={Love} /> by Rohan Lekhwani{" "}
+          </p>
+        </Row>
         <Row className="align-items-center text-center">
           <Col lg={5} className="text-lg-left">
             {copyright}
-          </Col>
-          {/* <Col lg={3} className="my-3 my-lg-0">
-            {twitter ? <SocialIcons.Twitter userName={twitter} /> : null}
-            {scholar ? <SocialIcons.Facebook userName={scholar} /> : null}
-            {linkedin ? <SocialIcons.Linkedin userName={linkedin} /> : null}
-            {github ? <SocialIcons.Github userName={github} /> : null}
-            {medium ? <SocialIcons.Medium userName={medium} /> : null}
-          </Col> */}
-          <Col lg={4} className="text-lg-right">
-            <a className="mr-3" href={privacyHref}>
-              {privacyText}
-            </a>
-            <a href={termsHref}>{termsText}</a>
           </Col>
         </Row>
       </Container>
